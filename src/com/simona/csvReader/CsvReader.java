12fbc44690afed36ab93bc27c4afa7a24b293374
Sorin Panduru <sorin.panduru@emag.ro>
2021-10-30 16:09:19 +0300

    final logger in csv reader
    Signed-off-by: Sorin Panduru <sorin.panduru@emag.ro>

--- src/com/simona/csvReader/CsvReader.java
+++ src/com/simona/csvReader/CsvReader.java
@@ -4,9 +4,13 @@ import java.util.logging.Logger;
 
 public class CsvReader {
 
-    private Logger logger;
+    final private Logger logger;
 
     public CsvReader(String filename){
         logger = Logger.getLogger("CsvReader");
+    }
+
+    public void read() {
+        logger.info("salut");
     }
 }
