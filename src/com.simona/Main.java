d1ad294bec12374ad260f4df94152f9eefbd29dd
Sorin Panduru <sorinpanduru@gmail.com>
2021-10-30 15:25:28 +0300

    initial commit
    Signed-off-by: Sorin Panduru <sorin.panduru@emag.ro>

---
+++ src/com.simona/Main.java
@@ -0,0 +1,10 @@
+package com.simona;
+
+public class Main {
+
+    public static String version = "v0.0.1";
+
+    public static void main(String[] args) {
+        System.out.println("Revolut Statement Parser " + version);
+    }
+}
